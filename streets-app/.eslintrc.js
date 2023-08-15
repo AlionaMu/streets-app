09c2cfd95e4c49fe862f879e45119fd1f9d3595f
AlionaMu <alionazimaby@gmail.com>
2023-08-15 15:52:01 +0300

    feat: add album component

---
+++ streets-app/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": {
+        "es6": true
+    },
+    "rules": {
+        "no-var": "error",
+        "indent": "off",
+        "no-multi-spaces": "error",
+        "no-multiple-empty-lines": "error",
+        "prefer-const": "error",
+        "no-use-before-define": "error"
+    }
+};
